56e58c7fe5b097f3518805c152b74cfae9430db6
jerson8moreno <jerson8moreno@gmail.com>
2021-12-08 22:11:41 -0500

    Agregar archivos de proyecto.

---
+++ GestionUsuario.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GestionUsuario.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
